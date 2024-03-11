437c7d63572fe6fbf9d6860af915bdbee6727674
Akash Nishad <akashnishad7054@gmail.com>
2024-03-11 13:54:32 +0530

    add some pages

--- lib/features/shop/models/product_model.dart
+++ lib/features/shop/models/product_model.dart
@@ -1,3 +1,6 @@
+import 'dart:js_interop';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shop/features/shop/models/brand_model.dart';
 import 'package:shop/features/shop/models/product_attribute_model.dart';
 import 'package:shop/features/shop/models/product_variation_model.dart';
@@ -11,14 +14,79 @@ class ProductModel {
   DateTime? date;
   double salePrice;
   String thumbnail;
-  boot? isFeatured;
+  bool? isFeatured;
   BrandModel? brand;
   String? description;
   String? categoryId;
-  List<String>? inages;
+  List<String>? images;
   String productType;
   List<ProductAttributeModel>? productAttributes;
   List<ProductVariationModel>? productVariations;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.stock,
+    required this.price,
+    required this.thumbnail,
+    required this.productType,
+    this.sku,
+    this.brand,
+    this.date,
+    this.images,
+    this.salePrice = 0.0,
+    this.isFeatured,
+    this.categoryId,
+    this.description,
+    this.productAttributes,
+    this.productVariations,
+  });
+
+  //create empty func for clean code
+  static ProductModel empty() => ProductModel(id: '', title: '', stock: 0, price: 0, thumbnail: '', productType: '');
+
+  //json formate
+  toJson(){
+    return {
+      'SKU' : sku,
+      'Title' : title,
+      'Stock' : stock,
+      'Price' : price,
+      'Images' : images ?? [],
+      'Thumbnail' : thumbnail,
+      'SalePrice' : salePrice,
+      'IsFeatured' : isFeatured,
+      'CategoryId' : categoryId,
+      'Brand' : brand!.toJson(),
+      'Description' : description,
+      'ProductType' : productType,
+      'ProductAttributes' : productAttributes != null ? productAttributes!.map((e) => e.toJson()).toList() : [],
+      'ProductVariations' : productVariations != null ? productVariations!.map((e) => e.toJson()).toList() : [],
+    };
+  }
+
+  /// Map Json oriented document snapshot from Firebase to Model
+  factory ProductModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+    final data = document.data()!;
+    return ProductModel(
+    id: document.id,
+    sku: data['SKU'],
+    title: data['Title'],
+    stock: data['Stock'] ?? 0,
+    isFeatured: data['IsFeatured'] ?? false,
+    price: double.parse((data['Price'] ?? 0.0).toString()),
+    salePrice: double.parse((data['SalePrice'] ?? 0.0).toString()),
+    thumbnail: data['Thumbnail'] ?? '',
+    categoryId: data['CategoryId'] ?? '',
+    description: data['Description'] ?? '',
+    productType: data['ProductType'] ?? '',
+    brand: BrandModel.fromJson(data['Brand']),
+    images: data['Images'] != null ? List<String>.from(data['Images']): [],
+    productAttributes: (data['ProductAttributes'] as List<dynamic>).map((e) => ProductAttributeModel.fromJson(e)).toList(),
+    productVariations: (data['ProductVariations'] as List<dynamic>).map((e) => ProductVariationModel.fromJson(e)).toList(), );
+    // ProductModel
+  }
+
 
 
 }
